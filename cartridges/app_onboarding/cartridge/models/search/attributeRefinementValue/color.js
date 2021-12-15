a44745aacb8e87c030dbf3c5f74ddf04eee1fbc8
SPalchuk <serhii.palchuk@valtech.com>
2021-12-15 13:40:28 +0200

    updated gitignore to remove unwanted files from PR, fixed spacings in templates, refactored logic to check page version(desktop/mobile), mobile-check function moved to separate utils file

--- cartridges/app_onboarding/cartridge/models/search/attributeRefinementValue/color.js
+++ cartridges/app_onboarding/cartridge/models/search/attributeRefinementValue/color.js
@@ -5,14 +5,10 @@ var base = module.superModule
 
 
 function ColorRefinementValueWrapper(productSearch, refinementDefinition, refinementValue) {
-  var isMobile = function () {
-    var userAgent = request.httpHeaders.get('user-agent')
-    return isMobile = userAgent.indexOf('Mobile') !== -1
-  }
-
+  var isMobile = require("../../utils")
   base.call(this, productSearch, refinementDefinition, refinementValue)
-
   this.hitCount = refinementValue.hitCount
   this.isMobile = isMobile
+
 }
 module.exports = ColorRefinementValueWrapper;
