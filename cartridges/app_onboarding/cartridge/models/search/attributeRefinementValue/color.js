14e3e4cbcccdca1fd786df4a3bf70fb344ac396a
SPalchuk <serhii.palchuk@valtech.com>
2021-12-16 12:59:19 +0200

    changed code formatting. utils now calling from template

--- cartridges/app_onboarding/cartridge/models/search/attributeRefinementValue/color.js
+++ cartridges/app_onboarding/cartridge/models/search/attributeRefinementValue/color.js
@@ -5,10 +5,7 @@ var base = module.superModule
 
 
 function ColorRefinementValueWrapper(productSearch, refinementDefinition, refinementValue) {
-  var isMobile = require("../../utils")
   base.call(this, productSearch, refinementDefinition, refinementValue)
   this.hitCount = refinementValue.hitCount
-  this.isMobile = isMobile
-
 }
 module.exports = ColorRefinementValueWrapper;
