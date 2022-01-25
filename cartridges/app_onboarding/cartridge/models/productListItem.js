d314662494ff6065179038e030424eadfe9d5de6
SPalchuk <serhii.palchuk@valtech.com>
2022-01-25 14:49:30 +0200

    changed approach. Logic operations and transactions moved to helpers

--- cartridges/app_onboarding/cartridge/models/productListItem.js
+++ cartridges/app_onboarding/cartridge/models/productListItem.js
@@ -1,23 +1,10 @@
 'use strict'
 
 var base = module.superModule;
-var Site = require('dw/system/Site');
-var txn = require('dw/system/Transaction');
 
 module.exports = function productListItem(productListItemObject) {
 
     base.call(this, productListItemObject)
-    var timeToLeaveBase = Site.getCurrent().getCustomPreferenceValue('TimeToLeave')
-    var today = new Date();
-    today = Date.parse(today)
-    var creationDate = Date.parse(productListItemObject.creationDate)
-    var gap = today - creationDate
-    var timeToLeave = (timeToLeaveBase * 24 * 60 * 60 * 1000 - gap) / 1000 / 60 / 60 / 24
-    if (Math.floor(timeToLeave) - 100 < 0) {
-        txn.wrap(function () {
-            productListItemObject.getList().removeItem(productListItemObject)
-        });
-    } else {
-        this.productListItem.timeToLeave = Math.floor(timeToLeave)
-    }
+    this.productListItem.timeToLeave = productListItemObject.custom.timeToLeave
+
 }
